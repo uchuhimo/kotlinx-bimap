cf6a1a3c3b99abbf25d30db1533c1f3d6fa9b4da
uchuhimo <uchuhimo@outlook.com>
2017-07-13 22:46:41 +0800

    gradle: disable licenseHeaderFile, use IDE support of license

--- src/main/kotlin/com/uchuhimo/collections/MutableBiMap.kt
+++ src/main/kotlin/com/uchuhimo/collections/MutableBiMap.kt
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.uchuhimo.collections
 
 import com.google.common.collect.HashBiMap
